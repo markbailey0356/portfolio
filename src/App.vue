116c0a48d4ba94ce0f55de8a01e28ca84a24079d
Mark Bailey <markbailey0356@gmail.com>
2019-02-21 13:37:32 +1030

    Add tailwindcss plugin

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
+    <div class="h-screen w-screen bg-red"></div>
     <router-view />
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "assets/styles/tailwind.postcss";
+</style>
