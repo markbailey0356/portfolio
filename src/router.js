9cb2d7deaaa0da600f8887ef2d10c8efef5ef110
Mark Bailey <markbailey0356@gmail.com>
2019-02-21 12:35:12 +1030

    Clean out template

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 
 Vue.use(Router);
+
+import Home from "@/views/Home.vue";
 
 export default new Router({
   mode: "history",
@@ -10,17 +11,7 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
       component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
 });
